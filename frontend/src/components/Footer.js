33fab03d5d2269da8b2676e576a78ddb152376ea
K. Albert <al1working.com>
2022-04-13 17:21:29 +0300

    add header and footer in React

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = ({footer}) => {
+    return (
+        <footer>
+        <p> Разработано Корепановым Альбертом для gb.ru </p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+const Menu = ({header}) => {
+    return (
+        <header>
+            <a href="#">Главная</a>
+            <a href="#">Вход</a>
+        </header>
+    )
+}
+
+export default Menu
